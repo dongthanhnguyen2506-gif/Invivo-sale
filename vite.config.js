0c913ca9be12b50cc88f58c31b6e4c8217b7d4a4
dongthanhnguyen2506-gif <dongthanhnguyen2506@gmail.com>
2026-06-01 00:48:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'ctv-active': ['./src/ctvActive.js'],
+          'ctv-pool':   ['./src/ctvPool.js'],
+        }
+      }
+    }
+  }
 })
